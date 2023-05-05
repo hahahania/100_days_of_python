7c833379cc7c4b59ec842b8cff25a11652934415
hzwolinska <zwolinskahania2416@gmail.com>
2023-05-05 10:24:36 +0200

    adding a hangman game

---
+++ beginner_level/hangman_game/hangman.py
@@ -0,0 +1,38 @@
+from random import choice
+from hangman_art import stages, words
+import os
+from time import sleep
+
+chosen_word = [x for x in choice(words)]
+underscores = ["_" for i in range(len(chosen_word))]
+mistakes = 0
+end_game = False
+
+while not end_game:
+    if mistakes == 6 or "_" not in underscores:
+        end_game = True
+
+    user_input = (input("Please, enter a letter\t")).lower()
+
+    if user_input in underscores:
+        print(f"You have already guesses {user_input}")
+
+    if user_input in chosen_word:
+        for i in range(len(chosen_word)):
+            if chosen_word[i] == user_input:
+                underscores[i] = user_input
+                print(
+                    f'You chose letter "{user_input}", it is in the word, your word looks like that:'
+                )
+                print("".join(underscores))
+                print(f"Your status look like that:\n{stages[mistakes]}")
+    else:
+        mistakes += 1
+        print(
+            f'You chose letter "{user_input}", it is not in the word, your word looks like that:'
+        )
+        print("".join(underscores))
+        print(f"Your status look like that:\n{stages[mistakes]}")
+
+    sleep(3)
+    os.system("clear")
