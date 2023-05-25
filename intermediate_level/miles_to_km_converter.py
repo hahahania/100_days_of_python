8f2cca4021295514879f2f15e47d44fbe6a4a223
hzwolinska <zwolinskahania2416@gmail.com>
2023-05-25 10:16:11 +0200

    miles to km converter with GUI

---
+++ intermediate_level/miles_to_km_converter.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+
+window = tk.Tk()
+window.minsize(width= 300, height=100)
+
+text_input = tk.Entry(width=10)
+text_input.grid(row=0,column=1)
+
+miles = tk.Label(text='miles')
+miles.grid(row=0,column=2)
+
+to = tk.Label(text = 'is equal to')
+to.grid(row=1,column=0)
+
+result = tk.Label(text = '0')
+result.grid(row=1,column=1)
+
+km = tk.Label(text='km')
+km.grid(row=1,column=2)
+
+def convert():
+    m = float(text_input.get())
+    converted = str(round(m*1.6, 1))
+    result.config(text=converted)
+    
+
+button = tk.Button(text='Convert', command=convert)
+button.grid(row=2,column=1)
+
+
+window.mainloop()
