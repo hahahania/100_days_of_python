37a4ca7feac57d2a280992e79e6d631b581c2a52
hzwolinska <zwolinskahania2416@gmail.com>
2023-05-31 07:52:17 +0200

    New app: Password Menager with GUI

---
+++ intermediate_level/password_generator_gui/generator.py
@@ -0,0 +1,17 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+def create_password():
+    password = []
+    for i in range(8):
+        password.append(random.choice(letters))
+    for i in range(3):
+        password.append(random.choice(symbols))
+    for i in range(3):
+        password.append(random.choice(numbers))
+
+    random.shuffle(password)
+    return(''.join(password))
---
+++ intermediate_level/password_generator_gui/main.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+from generator import create_password
+
+WIDTH = 500
+HEIGHT = 500
+BG_COLOR = '#EEE3CB'
+FONT_COLOR = '#967E76'
+FONT = 'Courier New'
+INPUT_COLOR = '#E8DAC3'
+
+def new_password():
+    password = create_password()
+    v.set(password)
+
+
+def collect_data():
+    website = web_name.get()
+    username = user_name.get()
+    password = user_password.get()
+
+    if all([website, username, password]):
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {username} "
+                                                      f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open('data.txt', 'w') as file:
+                file.write(f'{website} | {username} | {password}\n')
+    else:
+        popup = tk.Tk()
+        label = tk.Label(popup, text=f'No field should be empty')
+        label.pack()
+        button = tk.Button(popup, text='Close', command=popup.destroy)
+        button.pack()
+
+
+window = tk.Tk()
+window.config(padx=50, pady=50, bg=BG_COLOR)
+window.title('PASSWORD MENAGER')
+
+v = tk.StringVar()
+
+img = Image.open('locker.gif').resize((100, 100))
+image = ImageTk.PhotoImage(img)
+
+bg = tk.Canvas(window, width=150, height=150,
+               bg=BG_COLOR, highlightthickness=0)
+bg.create_image(75, 50, image=image,)
+bg.grid(column=1, row=0, columnspan=3)
+
+web = tk.Label(text='Website: ', fg=FONT_COLOR,
+               bg=BG_COLOR, font=(FONT, 15)).grid(column=1, row=2, sticky='E', pady=5)
+
+web_name = tk.Entry(window, bg=INPUT_COLOR,
+                    borderwidth=0.5, highlightthickness=0, fg=FONT_COLOR, font=(FONT, 15)).grid(column=2, columnspan=2, row=2, sticky='WE')
+
+name = tk.Label(text='Email / Nickname: ', fg=FONT_COLOR,
+                bg=BG_COLOR, font=(FONT, 15)).grid(column=1, row=3, sticky='E', pady=5)
+
+user_name = tk.Entry(window, bg=INPUT_COLOR,
+                     borderwidth=0.5, highlightthickness=0, fg=FONT_COLOR, font=(FONT, 15)).grid(column=2, columnspan=2, row=3, sticky='WE')
+
+password = tk.Label(text='Password: ', fg=FONT_COLOR,
+                    bg=BG_COLOR, font=(FONT, 15)).grid(column=1, row=4, sticky='E', pady=5)
+
+user_password = tk.Entry(window, bg=INPUT_COLOR, textvariable=v,
+                         borderwidth=0.5, highlightthickness=0, fg=FONT_COLOR, font=(FONT, 15)).grid(column=2, row=4,)
+
+generate_password = tk.Button(window, text='Generate Password', bg=INPUT_COLOR,
+                              borderwidth=0.25, highlightthickness=0, fg=FONT_COLOR, font=(FONT, 15), command=new_password).grid(column=3, row=4)
+
+submit = tk.Button(window, text='Submit', bg=INPUT_COLOR,
+                   borderwidth=0.25, highlightthickness=0, fg=FONT_COLOR, font=(FONT, 15), command=collect_data).grid(row=5, column=2, pady=20)
+
+window.mainloop()
