a6319effa0d3e17eceea505172f04ea984072240
hzwolinska <zwolinskahania2416@gmail.com>
2023-06-11 08:19:55 +0200

    saving data in json format and adding search button

--- intermediate_level/password_generator_gui/main.py
+++ intermediate_level/password_generator_gui/main.py
@@ -3,13 +3,7 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 from generator import create_password
 import pyperclip
-
-WIDTH = 500
-HEIGHT = 500
-BG_COLOR = '#EEE3CB'
-FONT_COLOR = '#967E76'
-FONT = 'Courier New'
-INPUT_COLOR = '#E8DAC3'
+import json
 
 
 def new_password():
@@ -18,27 +12,56 @@ def new_password():
     v.set(password)
 
 
-def collect_data():
+def save_data():
     website = web_name.get()
     username = user_name.get()
     password = user_password.get()
+    new_data = {website: {'email': username, 'password': password}}
 
     if all([website, username, password]):
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {username} "
-                                       f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
+        try:
+            with open('data.json', 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open('data.json', 'w') as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
+            with open('data.json', 'w') as file:
+                json.dump(data, file, indent=4)
+        finally:
             web_name.delete(0, tk.END)
             user_name.delete(0, tk.END)
             user_password.delete(0, tk.END)
-            with open('data.txt', 'w') as file:
-                file.write(f'{website} | {username} | {password}\n')
     else:
-        popup = tk.Tk()
-        label = tk.Label(popup, text=f'No field should be empty')
-        label.pack()
-        button = tk.Button(popup, text='Close', command=popup.destroy)
-        button.pack()
+        messagebox.showinfo(
+            title='Error', message='You left some fields empty!')
 
+
+def load_data():
+    website = web_name.get()
+    try:
+        with open('data.json') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title='Error', message='There is no data file')
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(
+                title=website, message=f'Email : {email}\nPassword : {password}')
+        else:
+            messagebox.showinfo(
+                title='Error', message='There is no data about this website')
+
+
+WIDTH = 500
+HEIGHT = 500
+BG_COLOR = '#EEE3CB'
+FONT_COLOR = '#967E76'
+FONT = 'Courier New'
+INPUT_COLOR = '#E8DAC3'
 
 window = tk.Tk()
 window.config(padx=50, pady=50, bg=BG_COLOR)
@@ -60,7 +83,11 @@ web.grid(column=1, row=2, sticky='E', pady=5)
 
 web_name = tk.Entry(window, bg=INPUT_COLOR,
                     borderwidth=0.5, highlightthickness=0, fg=FONT_COLOR, font=(FONT, 15))
-web_name.grid(column=2, columnspan=2, row=2, sticky='WE')
+web_name.grid(column=2, row=2, sticky='WE')
+
+search = tk.Button(window, text='Search', bg=INPUT_COLOR,
+                   borderwidth=0.25, highlightthickness=0, fg=FONT_COLOR, font=(FONT, 15), command=load_data)
+search.grid(column=3, row=2)
 
 name = tk.Label(text='Email / Nickname: ', fg=FONT_COLOR,
                 bg=BG_COLOR, font=(FONT, 15))
@@ -83,7 +110,7 @@ generate_password = tk.Button(window, text='Generate Password', bg=INPUT_COLOR,
 generate_password.grid(column=3, row=4)
 
 submit = tk.Button(window, text='Submit', bg=INPUT_COLOR,
-                   borderwidth=0.25, highlightthickness=0, fg=FONT_COLOR, font=(FONT, 15), command=collect_data)
+                   borderwidth=0.25, highlightthickness=0, fg=FONT_COLOR, font=(FONT, 15), command=save_data)
 submit.grid(row=5, column=2, pady=20)
 
 window.mainloop()
